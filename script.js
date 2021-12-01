a3cb37b3bb18599cd6c7b7f86e5d796343e42446
bradleyjosephh <bradleyjosephh@gmail.com>
2021-12-01 00:21:18 -0800

    most recent weather dashboard

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 var cityname = document.getElementById("cityname")
 var cityBtn = document.getElementById("cityBtn")
 var apiID = "6a3005c7b36bce0175f1efa2abab187c"
-cityBtn.addEventListener("click", function(event) {
+// search event occurs
+cityBtn.addEventListener("click", function (event) {
     event.preventDefault()
     console.log("On click")
     var citySearch = cityname.value
     console.log(citySearch)
     currentWeather(citySearch)
 })
-
+// current weather api call
 function currentWeather(cityname) {
     var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiID}&units=imperial`
     fetch(apiURL)
-    .then(function(data){
-        return data.json()
-    }) .then(function(result){
-        console.log(result)
-        var htmlText = `<div class="card" style="width: 18rem;">
+        .then(function (data) {
+            return data.json()
+        }).then(function (result) {
+            console.log(result)
+            var lat = result.coord.lat
+            var lon = result.coord.lon
+            var htmlText = `<div class="card" style="width: 18rem;">
         <h5 class="card-title">${cityname}</h5>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item">Weather: ${result.main.temp}<span><img src="https://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png"></span></li>
+          <li class="list-group-item">Weather: ${result.main.temp}℉<span><img src="https://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png"></span></li>
           <li class="list-group-item">Description: ${result.weather[0].description}</li>
-          <li class="list-group-item">Humidity: ${result.main.humidity}</li>
-          <li class="list-group-item">Windspeed: ${result.wind.speed}</li>
+          <li class="list-group-item">Humidity: ${result.main.humidity}%</li>
+          <li class="list-group-item">Windspeed: ${result.wind.speed} mph</li>
         </ul>
       </div>`
 
-      document.querySelector(".weather-container").innerHTML = htmlText
+            document.querySelector(".weather-container").innerHTML = htmlText
 
-    })
-}
+            // 8 day forecast API call with lat lon
+            var onecallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiID}&units=imperial`
+            fetch(onecallApi)
+                .then(function (data) {
+                    return data.json()
+                }).then(function (result) {
+                    console.log(result)
+                    var daily = result.daily
+                    var htmlCode = ""
+                    for (let i =0; i < daily.length ; i++) {
+                        htmlCode += `<div class="card" style="width: 18rem;">
+                        <h5 class="card-title">Day${i+1}</h5>
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item">Weather: ${daily[i].temp.day}℉<span><img src="https://openweathermap.org/img/wn/${daily[i].weather[0].icon}@2x.png"></span></li>
+                          <li class="list-group-item">Description: ${daily[i].weather[0].description}</li>
+                          <li class="list-group-item">Humidity: ${daily[i].humidity}%</li>
+                          <li class="list-group-item">Windspeed: ${daily[i].wind_speed} mph</li>
+                          <li class="list-group-item">UV-Index: ${daily[i].uvi}</li>
+                        </ul>
+                      </div>`
+                      
+                    }
+                    document.querySelector(".container-fiveForecast").innerHTML = htmlCode
+                })
+        })
+    }
